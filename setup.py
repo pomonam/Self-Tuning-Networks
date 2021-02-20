2c27865612ca67a75147c6abfdd9ce33f304e317
Juhan Bae <juhan.bae@mail.utoronto.ca>
2021-02-20 02:46:08 -0500

    package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="stn",
+    version="1.1",
+    packages=["src", "src.data", "src.hyper", "src.utils", "src.layers"],
+    url="",
+    license="",
+    author="Juhan Bae",
+    author_email="jbae@utoronto.ca",
+    description="Self-Tuning Networks"
+)
